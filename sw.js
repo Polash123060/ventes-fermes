4020edaa3143010a5c53068060fa8903a1035f1a
Polash123060 <polashkanti46@gmail.com>
2025-11-04 17:50:57 +0100

    Delete sw.js

--- sw.js
+++
@@ -1,8 +0,0 @@
-
-const CACHE = 'ventes-ferme-v2';
-const FILES = ['./ventes_app.html','./manifest.webmanifest','./icons/icon-192.png','./icons/icon-512.png'];
-self.addEventListener('install', e=>{ e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES))); self.skipWaiting(); });
-self.addEventListener('activate', e=>{ e.waitUntil(self.clients.claim()); });
-self.addEventListener('fetch', e=>{
-  e.respondWith(caches.match(e.request).then(r=> r || fetch(e.request)));
-});
